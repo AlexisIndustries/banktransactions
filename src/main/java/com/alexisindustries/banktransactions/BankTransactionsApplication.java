9c70025dd6e28775165398c9e1f533b4800b198a
AlexisIndustries <alexeybobrovich21@gmail.com>
2025-01-25 20:11:33 +0300

    Initial Commit

---
+++ src/main/java/com/alexisindustries/banktransactions/BankTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.alexisindustries.banktransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class BankTransactionsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexisindustries/banktransactions/BankTransactionsApplicationTests.java
@@ -0,0 +1,10 @@
+package com.alexisindustries.banktransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionsApplicationTests {
+
+
+}
